059c86be4c7b1a7698e1390522a1d8a251c04003
Kim Jong Hyun <kimjonghyun.develop@gmail.com>
2025-04-12 11:31:30 +0900

    add new IosLibrary

---
+++ ios/IosLibrary/IosLibraryTests/IosLibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  IosLibraryTests.swift
+//  IosLibraryTests
+//
+//  Created by Kim Jong Hyun on 2025/04/12.
+//
+
+import Testing
+@testable import IosLibrary
+
+struct IosLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
