79ddfc2b77c43f9316e93b4505bf6195537f2911
Kim Jong Hyun <kimjonghyun.develop@gmail.com>
2025-04-20 15:01:28 +0900

    add new MacLibraryExample、MacLibrary、UnityMacPlugin projects

---
+++ mac/MacLibraryExample/MacLibraryExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacLibraryExample
+//
+//  Created by Kim Jong Hyun on 2025/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac/MacLibraryExample/MacLibraryExample/MacLibraryExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLibraryExampleApp.swift
+//  MacLibraryExample
+//
+//  Created by Kim Jong Hyun on 2025/04/20.
+//
+
+import SwiftUI
+
+@main
+struct MacLibraryExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
