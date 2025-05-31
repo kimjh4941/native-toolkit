6a2251b87801db8496ac3b8e2c99054c7cf27f27
Kim JongHyun <kimjonghyun.develop@gmail.com>
2025-05-31 13:45:51 +0900

    [windows] add new WindowsLibraryExample Project add new WindowsLibrary Project add new UnityWindowsPlugin Project

---
+++ windows/WindowsLibrary/WindowsLibrary.cpp
@@ -0,0 +1,127 @@
+﻿// WindowsLibrary.cpp : DLL の初期化ルーチンを定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "WindowsLibrary.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+//
+//TODO: この DLL が MFC DLL に対して動的にリンクされる場合、
+//		MFC 内で呼び出されるこの DLL からエクスポートされたどの関数も
+//		関数の最初に追加される AFX_MANAGE_STATE マクロを
+//		持たなければなりません。
+//
+//		例:
+//
+//		extern "C" BOOL PASCAL EXPORT ExportedFunction()
+//		{
+//			AFX_MANAGE_STATE(AfxGetStaticModuleState());
+//			// 通常関数の本体はこの位置にあります
+//		}
+//
+//		このマクロが各関数に含まれていること、MFC 内の
+//		どの呼び出しより優先することは非常に重要です。
+//		it は、次の範囲内で最初のステートメントとして表示されるべきです
+//		らないことを意味します、コンストラクターが MFC
+//		DLL 内への呼び出しを行う可能性があるので、オブ
+//		ジェクト変数の宣言よりも前でなければなりません。
+//
+//		詳細については MFC テクニカル ノート 33 および
+//		58 を参照してください。
+//
+
+// CWindowsLibraryApp
+
+BEGIN_MESSAGE_MAP(CWindowsLibraryApp, CWinApp)
+END_MESSAGE_MAP()
+
+
+// CWindowsLibraryApp の構築
+
+CWindowsLibraryApp::CWindowsLibraryApp()
+{
+	// TODO: この位置に構築用コードを追加してください。
+	// ここに InitInstance 中の重要な初期化処理をすべて記述してください。
+}
+
+
+// 唯一の CWindowsLibraryApp オブジェクト
+
+CWindowsLibraryApp theApp;
+
+const GUID CDECL _tlid = {0x4de616bc,0x18d3,0x4763,{0x87,0x54,0x06,0xf0,0x5a,0x9d,0x08,0x33}};
+const WORD _wVerMajor = 1;
+const WORD _wVerMinor = 0;
+
+
+// CWindowsLibraryApp の初期化
+
+BOOL CWindowsLibraryApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+	// すべての OLE サーバー ファクトリを実行中に登録してください。これにより、
+	//  OLE ライブラリが他のアプリケーションからオブジェクトを作成できるようになります。
+	COleObjectFactory::RegisterAll();
+
+	return TRUE;
+}
+
+// DllGetClassObject - クラス ファクトリを返します。
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	return AfxDllGetClassObject(rclsid, riid, ppv);
+}
+
+
+// DllCanUnloadNow - COM が DLL をアンロードできるようにします。
+
+STDAPI DllCanUnloadNow(void)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	return AfxDllCanUnloadNow();
+}
+
+
+// DllRegisterServer - エントリをシステム レジストリに追加します。
+
+STDAPI DllRegisterServer(void)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	if (!AfxOleRegisterTypeLib(AfxGetInstanceHandle(), _tlid))
+		return SELFREG_E_TYPELIB;
+
+	if (!COleObjectFactory::UpdateRegistryAll())
+		return SELFREG_E_CLASS;
+
+	return S_OK;
+}
+
+
+// DllUnregisterServer - エントリをレジストリから削除します。
+
+STDAPI DllUnregisterServer(void)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	if (!AfxOleUnregisterTypeLib(_tlid, _wVerMajor, _wVerMinor))
+		return SELFREG_E_TYPELIB;
+
+	if (!COleObjectFactory::UpdateRegistryAll(FALSE))
+		return SELFREG_E_CLASS;
+
+	return S_OK;
+}
---
+++ windows/WindowsLibrary/WindowsLibrary.h
@@ -0,0 +1,27 @@
+﻿// WindowsLibrary.h : WindowsLibrary DLL のメイン ヘッダー ファイル
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CWindowsLibraryApp
+// このクラスの実装に関しては WindowsLibrary.cpp をご覧ください
+//
+
+class CWindowsLibraryApp : public CWinApp
+{
+public:
+	CWindowsLibraryApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
